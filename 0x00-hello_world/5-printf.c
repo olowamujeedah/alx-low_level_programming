3fd9a08cd45604a024cb1260aaae511ceb2815b5
mujeedah <mujeedaholowa@gmail.com>
2022-03-12 13:30:09 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * main- printsbthe phrase with proper grammar
+ * Return- 0 if exited properly
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n")
+		return (0);
+}
