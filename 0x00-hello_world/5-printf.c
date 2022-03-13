81cf9d8af3a81b5e1e034a4aa3796cac54c6a6a9
mujeedah <mujeedaholowa@gmail.com>
2022-03-13 06:33:38 -0700

    print c lang

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  * main- printsbthe phrase with proper grammar
- * Return- 0 if exited properly
- * */
+ * Return: 0 if exited properly
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art, \n")
