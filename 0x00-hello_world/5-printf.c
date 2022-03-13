6ce7e367d3bb460a52131c9e7cda615b8431c619
mujeedah <mujeedaholowa@gmail.com>
2022-03-13 07:24:36 -0700

    betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main- printsbthe phrase with proper grammar
+ * description : print the word; with proper gramma
  * Return: 0 if exited properly
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
