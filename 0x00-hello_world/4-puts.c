cf5e843195fe98208ff09f5b2c98bb00fa98aaa6
mujeedah <mujeedaholowa@gmail.com>
2022-03-13 22:42:36 -0700

    c lang

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+/**
+*main- print the words. write a function that print the following words
+*description - print the words in quote
+*Return: zero (success)
+*/
 int main(void)
 {
-/*
-main- print the words. write a function that print the following words and let it return back to zero. then, returb to zero
-*/	
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
